4c6d17fd0fffa266aeb1b1b9400b533588d65e61
reconrus <reconrus@yandex.ru>
2018-04-13 02:36:21 +0300

    floating numbers

--- picture_generator.v
+++ picture_generator.v
@@ -4,6 +4,7 @@ module picture_generator(clk, vga_h_sync, vga_v_sync, vga_R, vga_G, vga_B);
 
 //input reset;
 input clk;
+//input char;
 output vga_h_sync, vga_v_sync;
 output [3:0] vga_R;
 output [3:0] vga_G;
@@ -54,27 +55,45 @@ reg [11:0] rgb_data;
 parameter VIDEO_W	= 640;
 parameter VIDEO_H	= 480;
 
-always@(posedge clk)
-begin
-//rgb_data <= {4'hf, 4'h0, 4'h0};
-    begin
-      if (y > 0 && y <= VIDEO_H/3 && 0<x && x <= VIDEO_W)
-					rgb_data <= {4'h0, 4'h0, 4'hf}; 
-				else if (y > VIDEO_H/3 && y <= VIDEO_H*2/3 && 0<x && x <= VIDEO_W )
-					rgb_data <= {4'hf,4'hf, 4'hf};  
-				else if(y > VIDEO_H*2/3 && y <=VIDEO_H && 0<x && x <= VIDEO_W)
-					rgb_data <= {4'hf, 4'h0, 4'h0}; 
-				else rgb_data <= 12'h000; 
- 
-    end
-end
+
+//
+//https://github.com/bogini/Pong/blob/master/font_test_gen.v#L22
+// signal declaration
+wire [10:0] rom_addr;
+wire [6:0] char_addr;
+wire [3:0] row_addr;
+wire [2:0] bit_addr;
+wire [7:0] font_word;
+wire font_bit, text_bit_on;
+
+// body
+// instantiate font ROM
+font_rom font_unit
+(.clk(clk), .addr(rom_addr), .data_reg(font_word));
+// font ROM interface
+assign char_addr = {y[5:4], x[7:3]};
+assign row_addr = y[3:0];
+assign rom_addr = {char_addr, row_addr};
+assign bit_addr = x[2:0];
+assign font_bit = font_word[~bit_addr];
+// "on" region limited to top-left corner
+assign text_bit_on = (x[9:8]==0 && y[9:6]==0) ?
+                      font_bit : 1'b0;
+ // rgb multiplexing circuit
+ always@(*)
+   if (~in_display_area)
+     rgb_data = 12'h000; // blank
+   else
+     if (text_bit_on)
+        rgb_data = 12'hfff;  // white
+     else
+        rgb_data = 12'h000;  // black
+
+//
 
 assign vga_R=rgb_data[11:8];
 assign vga_G=rgb_data[7:4];
 assign vga_B=rgb_data[3:0]; 
 
-//assign vga_R = 4'hf;
-//assign vga_G = 4'h0;
-//assign vga_B = 4'h0;
 
 endmodule
