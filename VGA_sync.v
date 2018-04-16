56f5fc000de0783a1f3e94969909fe868de85c36
reconrus <reconrus@yandex.ru>
2018-04-16 16:07:32 +0300

    not floating netherlands flag

--- VGA_sync.v
+++ VGA_sync.v
@@ -1,10 +1,12 @@
 //http://www.fpga4fun.com/PongGame.html
 
-module VGA_sync(clk, vga_h_sync, vga_v_sync, in_display_area);
+module VGA_sync(clk, vga_h_sync, vga_v_sync, in_display_area, x, y);
 input clk;
 output reg vga_h_sync;
 output reg vga_v_sync;
 output reg in_display_area;
+output reg[9:0] x;
+output reg[9:0] y;
 
 reg [9:0] h;
 reg [9:0] v;
@@ -18,7 +20,7 @@ parameter v_front = 10;
 parameter H_sync_cycle = 96;
 parameter V_sync_cycle = 2;
 
-always @(negedge clk)
+always @(posedge clk)
 begin
 //	if(reset)
 //		begin
@@ -55,7 +57,20 @@ always@(negedge clk)
 begin
 	vga_h_sync <= vga_HS;
 	vga_v_sync <= vga_VS;
-	in_display_area <= on_display;
+	in_display_area <= on_display;	
+	
+	if(h >= (h_line - h_front))
+		x <= 0;
+	else 
+		if(h < h_back)
+			x <= 0;
+		else x <= h - h_back;
+	if(v >= (v_line - v_front))
+		y <= 0;
+	else 
+		if(v < v_back)
+			y <= 0;
+		else y <= v - v_back;
 end
-
+	
 endmodule
--- picture_generator.v
+++ picture_generator.v
@@ -14,82 +14,75 @@ wire in_display_area;
 wire [9:0] h;
 wire [9:0] v;
 
-VGA_sync sync_gen(.clk(clk), .vga_h_sync(vga_h_sync), .vga_v_sync(vga_v_sync), 
-						.in_display_area(in_display_area));
-			
+reg [9:0] x;
+reg [9:0] y;
 
-reg [18:0] x;
-reg [18:0] y;	
-reg was_next_lined;
-	
-////Coordinates generator
-always@(posedge clk)
-begin
-  
-   if (in_display_area==1'b1)
-   begin
-     x<=x+1;
-	  was_next_lined <= 0;
-	end
-	else
-	   begin 
-			if(y < VIDEO_H && !was_next_lined)
-				begin
-				y <= y + 1; 
-				was_next_lined <= 1;
-				end
-			else 
-			begin
-				if(!was_next_lined)
-					begin
-					y <= 19'd0;
-					was_next_lined <= 1;
-					end
-			x<=19'd0;
-			end
-		end
-end	
+VGA_sync sync_gen(.clk(clk), .vga_h_sync(vga_h_sync), .vga_v_sync(vga_v_sync), 
+						.in_display_area(in_display_area), .x(h), .y(v));
+		
+//reg was_next_lined;
 		
 reg [11:0] rgb_data;
 
 parameter VIDEO_W	= 640;
 parameter VIDEO_H	= 480;
 
+always@(*)
+begin
+	x <= h;
+	y <= v;
+end
 
+
+
+////
+////https://github.com/bogini/Pong/blob/master/font_test_gen.v#L22
+//// signal declaration
+//wire [10:0] rom_addr;
+//wire [6:0] char_addr;
+//wire [3:0] row_addr;
+//wire [2:0] bit_addr;
+//wire [7:0] font_word;
+//wire font_bit, text_bit_on;
 //
-//https://github.com/bogini/Pong/blob/master/font_test_gen.v#L22
-// signal declaration
-wire [10:0] rom_addr;
-wire [6:0] char_addr;
-wire [3:0] row_addr;
-wire [2:0] bit_addr;
-wire [7:0] font_word;
-wire font_bit, text_bit_on;
-
-// body
-// instantiate font ROM
-font_rom font_unit
-(.clk(clk), .addr(rom_addr), .data_reg(font_word));
-// font ROM interface
-assign char_addr = {y[5:4], x[7:3]};
-assign row_addr = y[3:0];
-assign rom_addr = {char_addr, row_addr};
-assign bit_addr = x[2:0];
-assign font_bit = font_word[~bit_addr];
-// "on" region limited to top-left corner
-assign text_bit_on = (x[9:8]==0 && y[9:6]==0) ?
-                      font_bit : 1'b0;
- // rgb multiplexing circuit
- always@(*)
-   if (~in_display_area)
-     rgb_data = 12'h000; // blank
-   else
-     if (text_bit_on)
-        rgb_data = 12'hfff;  // white
-     else
-        rgb_data = 12'h000;  // black
-
+//// body
+//// instantiate font ROM
+//font_rom font_unit
+//(.clk(clk), .addr(rom_addr), .data_reg(font_word));
+//// font ROM interface
+//assign char_addr = {y[5:4], x[7:3]};
+//assign row_addr = y[3:0];
+//assign rom_addr = {char_addr, row_addr};
+//assign bit_addr = x[2:0];
+//assign font_bit = font_word[~bit_addr];
+//// "on" region limited to top-left corner
+//assign text_bit_on = (x[9:6]==0 && y[9:6]==0) ?
+//                      font_bit : 1'b0;
+// // rgb multiplexing circuit
+// always@(*)
+//   if (~in_display_area)
+//     rgb_data = 12'h000; // blank
+//   else
+//     if (text_bit_on)
+//        rgb_data = 12'hfff;  // white
+//     else
+//        rgb_data = 12'h000;  // black
 //
+////
+
+
+always@(posedge clk)
+begin
+    begin
+			if (y > 0 && y <= VIDEO_H/3 && 0<x && x <= VIDEO_W)
+					rgb_data <= {4'h0, 4'h0, 4'hf}; 
+			else if (y > VIDEO_H/3 && y <= VIDEO_H*2/3 && 0<x && x <= VIDEO_W )
+ 					rgb_data <= {4'hf,4'hf, 4'hf};  
+			else if(y > VIDEO_H*2/3 && y <=VIDEO_H && 0<x && x <= VIDEO_W)
+ 					rgb_data <= {4'hf, 4'h0, 4'h0}; 
+ 				else rgb_data <= 12'h000; 
+	end
+end
 
 assign vga_R=rgb_data[11:8];
 assign vga_G=rgb_data[7:4];
