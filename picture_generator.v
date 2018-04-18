a550e29bd7e5bda1f3a83f97543610e5c5a714ff
reconrus <reconrus@yandex.ru>
2018-04-18 15:24:48 +0300

    first line

--- picture_generator.v
+++ picture_generator.v
@@ -1,78 +1,76 @@
 //http://www.fpga4fun.com/PongGame.html
 
-module picture_generator(clk, vga_h_sync, vga_v_sync, vga_R, vga_G, vga_B);
+module picture_generator
+#(parameter maxInput = 16,
+  parameter VIDEO_W	= 640,
+  parameter VIDEO_H	= 480)
+(clk, numbers, vga_h_sync, vga_v_sync, vga_R, vga_G, vga_B);
 
-//input reset;
 input clk;
-//input char;
+input [15:0] numbers;
+
 output vga_h_sync, vga_v_sync;
 output [3:0] vga_R;
 output [3:0] vga_G;
 output [3:0] vga_B;
 
 wire in_display_area;
-wire [9:0] h;
-wire [9:0] v;
+wire [9:0] x;
+wire [9:0] y;
 
-reg [9:0] x;
-reg [9:0] y;
+integer index;
 
 VGA_sync sync_gen(.clk(clk), .vga_h_sync(vga_h_sync), .vga_v_sync(vga_v_sync), 
-						.in_display_area(in_display_area), .x(h), .y(v));
-		
-//reg was_next_lined;
-		
+						.in_display_area(in_display_area), .x(x), .y(y));
+				
 reg [11:0] rgb_data;
-
-parameter VIDEO_W	= 640;
-parameter VIDEO_H	= 480;
-
-always@(*)
-begin
-	x <= h;
-	y <= v;
-end
-
-
 
 //
 //https://github.com/bogini/Pong/blob/master/font_test_gen.v#L22
 // signal declaration
-wire [10:0] rom_addr;
 reg [5:0] char_addr;
+wire [10:0] rom_addr;
 wire [3:0] row_addr;
 wire [2:0] bit_addr;
 wire [7:0] font_word;
 wire font_bit, text_bit_on;
-
 
 // body
 // instantiate font ROM
 font_rom font_unit
 (.clk(clk), .addr(rom_addr), .data_reg(font_word));
 // font ROM interface
-//wire char[5:0];
-//assign char = {y[5:4], x[5:3]};
+
 assign row_addr = y[3:0];
 assign rom_addr = {char_addr, row_addr};
 assign bit_addr = x[2:0];
 assign font_bit = font_word[~bit_addr];
 
 always@(*)
-case(x[5:3])
+begin
+if(x == 0 || index >= maxInput)
+	index = 0;
+else if(x % 8 == 0)
+	index = index + 4;
+	
+case(numbers[index +: 3])
 
-	0: char_addr <= 6'h31;
-	1: char_addr <= 6'h37;
-	2: char_addr <= 6'h00;
-	3: char_addr <= 6'h30;
-	4: char_addr <= 6'h34;
-	5: char_addr <= 6'h00;
-	6: char_addr <= 6'h31;
-	7: char_addr <= 6'h38;
-
+	4'h0: char_addr <= 6'h30;
+	4'h1: char_addr <= 6'h31;
+	4'h2: char_addr <= 6'h32;
+	4'h3: char_addr <= 6'h33;
+	4'h4: char_addr <= 6'h34;
+	4'h5: char_addr <= 6'h35;
+	4'h6: char_addr <= 6'h36;
+	4'h7: char_addr <= 6'h37;
+	4'h8: char_addr <= 6'h38;
+	4'h9: char_addr <= 6'h39;
+	
 endcase
 
-assign text_bit_on =  (x[9:6] == 0 && y[9:4] == 0)? font_bit : 1'b0;
+end
+
+assign text_bit_on =  (x[9:7] == 0 && y[9:4] == 0)? font_bit : 1'b0;
 
  // rgb multiplexing circuit
  always@(*)
@@ -84,21 +82,6 @@ assign text_bit_on =  (x[9:6] == 0 && y[9:4] == 0)? font_bit : 1'b0;
      else
         rgb_data = 12'h000;  // black
 
-//
-
-
-//always@(posedge clk)
-//begin
-//    begin
-//			if (y > 0 && y <= VIDEO_H/3 && 0<x && x <= VIDEO_W)
-//					rgb_data <= {4'h0, 4'h0, 4'hf}; 
-//			else if (y > VIDEO_H/3 && y <= VIDEO_H*2/3 && 0<x && x <= VIDEO_W )
-// 					rgb_data <= {4'hf,4'hf, 4'hf};  
-//			else if(y > VIDEO_H*2/3 && y <=VIDEO_H && 0<x && x <= VIDEO_W)
-// 					rgb_data <= {4'hf, 4'h0, 4'h0}; 
-// 				else rgb_data <= 12'h000; 
-//	end
-//end
 
 assign vga_R=rgb_data[11:8];
 assign vga_G=rgb_data[7:4];
